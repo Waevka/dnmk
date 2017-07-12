3a6da4a13c0198a090082c8e585dce543ac65b30
Waevka <waveswallow@gmail.com>
2017-07-12 23:29:45 +0200

    Updated filenames to uppercase standard.

---
+++ dnmk/Assets/Scripts/DnmkEditor/PlayingFieldGizmo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayingFieldGizmo : MonoBehaviour {
+    public DnmkPlayingField playingField;
+
+    void OnDrawGizmos()
+    {
+        if (playingField != null)
+        {
+            Vector3 playingFieldCenter = playingField.transform.position;
+            Vector3 topLeftCorner       = new Vector3(playingFieldCenter.x + playingField.Width / 2.0f, playingFieldCenter.y - playingField.Height / 2.0f, 0.0f);
+            Vector3 topRightCorner      = new Vector3(playingFieldCenter.x + playingField.Width / 2.0f, playingFieldCenter.y + playingField.Height / 2.0f, 0.0f);
+            Vector3 bottomLeftCorner    = new Vector3(playingFieldCenter.x - playingField.Width / 2.0f, playingFieldCenter.y - playingField.Height / 2.0f, 0.0f);
+            Vector3 bottomRightCorner   = new Vector3(playingFieldCenter.x - playingField.Width / 2.0f, playingFieldCenter.y + playingField.Height / 2.0f, 0.0f);
+
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(topLeftCorner, topRightCorner);
+            Gizmos.DrawLine(topRightCorner, bottomRightCorner);
+            Gizmos.DrawLine(bottomRightCorner, bottomLeftCorner);
+            Gizmos.DrawLine(bottomLeftCorner, topLeftCorner);
+        }
+    }
+}
